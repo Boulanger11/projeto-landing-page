9cde1c867be1a24f5f0a7cde7a7a05e2b2cb98da
Boulanger11 <leo.tcheco@gmail.com>
2023-05-01 18:26:53 -0300

    funcionalidade

---
+++ script.js
@@ -0,0 +1,18 @@
+const botao1 = document.getElementById(`meubotao-1`)
+
+const irParaFormulario = (event) => {
+    event.preventDefault();
+    document.getElementById(`meuFormulario`).scrollIntoView({behavior: `smooth`})
+}
+
+const reservar = (event) =>{
+    event.preventDefault();
+    const emailInput = document.getElementById(`E-mail`)
+    const email = emailInput.value
+
+    if (email.length > 0) {
+        alert(`Entraremos em contato através do ${email}`)
+    }else {
+        alert(`Por favor, digite um e-mail valido.`)
+    }
+}
